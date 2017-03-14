907cc3b888228c403135da49452de69493f3279b
Haruna Popoola <haruna.popoola@yahoo.com>
2017-03-14 21:35:45 +0100

    Made the Command Line Output Beautiful

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
+'use strict'
 
+const figlet = require('figlet');
+const chalk = require('chalk');
 // Import the lib for all functions
 const lib = require('./lib/lib');
 
-/*
- *  getLocation displays prompt 
- *  asking the user to write the location 
- *  returns a string
- */
-lib.getLocation(function(result){
+// dislay the figlet Header
+figlet('LATLNG', function(err, data) {
 
-	// user input is saved in location
-	var location = result.location;
+	if (err) {
+	  console.log('Something went wrong...');
+	  console.dir(err);
+	  return;
+	}
+	console.log();
+    console.log(chalk.gray(data));
+    console.log();
 
-	/*
-	 *  getLatLng connects to the Google Map API
-	 *  and search for details of the location
+    /*
+	 *  getLocation displays prompt 
+	 *  asking the user to write the location 
+	 *  returns a string
 	 */
-	lib.getLatLng(location);
+	lib.getLocation(function(result){
+
+		// user input is saved in location
+		var location = result.location;
+
+		/*
+		 *  getLatLng connects to the Google Map API
+		 *  and search for details of the location
+		 */
+		lib.getLatLng(location);
+	});
+
+      
 });
